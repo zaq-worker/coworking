19decccf58eb978adc1368c701647a9e9496d0e2
yasushi miyazaki <yasushi1002-s14@outlook.com>
2022-02-07 18:59:55 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/style.css">
+        <title>TECH I.S. Coworking - テックアイエスコワーキング</title>
+    </head>
+    <body>
+    <header class="header">
+        <div class="logo">
+            <h1><img src="img/logo.png" alt="ロゴ画像"></h1>
+        </div>
+        <ul class="menu">
+            <li class="menu-item"><a href="https://forms.gle/dKHLvPUtbcNNEAfR6">見学・申し込みはこちらから</a></li>
+        </ul>
+    </header>
+
+    <main>
+            <div class="front-ig">
+            <a href="https://www.instagram.com/tech.i.s.coworking/"><img src="img/logo_ig.png" alt="Instagram_ロゴ画像"></a>
+        </div>
+
+        <section class="front-fv">
+            <picture class="front-fv__img">
+                <source srcset="img/mobile-fv.png" alt="トップ画像" media="(max-width: 800px)">
+                <img src="img/fv.png" alt="トップ画像">
+            </picture>
+            <div class="front-fv__desc">
+                <div class="front-fv__desc-detail">
+                    <span class="front-fv__subtitle">松山市駅から徒歩5分</span>
+                    <h2 class="front-fv__maintitle">誰でも使える<br>コワーキング</h2>
+                    <time  class="front-fv__time">10:00~22:00</time>
+                    <small class="front-fv__text">当日のみの利用も月額利用もできます。</small>
+                </div>
+            </div>
+        </section>
+
+        <section class="front-about common-wrapper">
+            <div class="common-inner">
+                <div class="front-about__desc">
+                    <div class="front-about__text">
+                        <h3 class="front-about__title">
+                            誰でも使えるコワーキングで<br>未来を作ろう
+                        </h3>
+                        <p>テックアイエスコワーキングは松山市駅から徒歩５分で松山の中心、銀天街にあるコワーキングスペースです。愛媛で事業をされる方や起業される方の活動拠点となることで新たなイノベーションの起点となる空間を目指します。</p>
+                        <p>あなたも愛媛の中心地でイノベーションを起こしませんか？</p>
+                    </div>
+                    <div class="front-about__img">
+                        <img src="img/about.png" alt="1階スペース">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="front-facility common-wrapper">
+            <div class="common-inner">
+                <div class="common-title">
+                    <span class="common-subtitle">Facility</span>
+                    <h3 class="common-maintitle">テックアイエスコワーキング 愛媛県</h3>
+                </div>
+                <ul class="front-facility__icon">
+                    <li class="icon"><img src="img/online.png" alt="オンライン"></li>
+                    <li class="icon"><img src="img/drink.png" alt="ドリンク"></li>
+                    <li class="icon"><img src="img/print.png" alt="プリント"></li>
+                    <li class="icon"><img src="img/room.png" alt="会議室"></li>
+                    <li class="icon"><img src="img/charge.png" alt="電源あり"></li>
+                    <li class="icon"><img src="img/wifi.png" alt="wifi"></li>
+                    <li class="icon"><img src="img/event.png" alt="イベントスペース"></li>
+                </ul>
+            </div>
+            <div class="front-facility__space">
+                <img src="img/space.png" alt="コワーキング見取り図">
+            </div>
+        </section>
+
+        <section class="front-feature common-wrapper">
+            <div class="common-inner">
+                <div class="common-title">
+                    <span class="common-subtitle">Feature</span>
+                    <h3 class="common-maintitle">各スペースの紹介</h3>
+                </div>
+                <ul class="front-feature__list">
+                    <li class="front-feature__item">
+                        <img src="img/open.png" alt="オープンスペース1F">
+                        <h4>オープンスペース1F</h4>
+                        <p>来客の方や利用者の方とリラックスして打ち合わせすることができるスペースです。</p>
+                    </li>
+                    <li class="front-feature__item">
+                        <img src="img/open2.png" alt="自主スペース2F">
+                        <h4>自主スペース1F/2F</h4>
+                        <p>フリードリンク（アイス/ホット）でコーヒーを飲みながら、集中して作業することができるスペースです。</p>
+                    </li>
+                    <li class="front-feature__item">
+                        <img src="img/door.png" alt="オンラインルーム2F">
+                        <h4>オンラインルーム2F</h4>
+                        <p>誰にも見られない空間で、集中してテレワークすることができるスペースです。</p>
+                    </li>
+                </ul>
+                <p>※会議室、イベントスペースの時間貸しもありますので、お問合せ下さい。</p>
+                <div class="common-btn">
+                    <a href="https://forms.gle/dKHLvPUtbcNNEAfR6">見学申し込みはこちら</a>
+                </div>
+            </div>
+        </section>
+
+        <section class="front-flow common-wrapper">
+            <div class="common-inner">
+                <div class="common-title">
+                    <span class="common-subtitle">Flow</span>
+                    <h3 class="common-maintitle">お申し込みの流れ</h3>
+                </div>
+                <ul class="front-flow__list">
+                    <li class="front-flow__item">
+                        <h4>月額会員</h4>
+                        <p>ご利用に関する案内などのため、原則店頭で対面申込みとさせていただきます。 申込みは、オンラインで行います。（株式会社メタップスペイメントが提供する「会費ペイ」というサービスを使用しています。）
+                            まずはHPフォームからお問い合わせください。2営業日以内にお返事いたします。</p>
+                    </li>
+                    <li class="front-flow__item">
+                        <h4>単発利用(ドロップイン)</h4>
+                        <p>利用したい日にお越しください。 氏名・電話番号・利用時間をお伺いし、利用料金をお支払いいただきます。 その際に、出入りするためのカードをお渡しいたします。お帰りの際は、ポストにご返却いただくだけで簡単に退出可能です。</p>
+                    </li>
+                </ul>
+            </div>
+        </section>
+
+        <section class="front-price common-wrapper">
+            <div class="common-inner">
+                <div class="common-title">
+                    <span class="common-subtitle">Price</span>
+                    <h3 class="common-maintitle">料金（税金）</h3>
+                </div>
+                <table class="front-price__table">
+                    <tr>
+                        <th>月額会員</th>
+                        <td>
+                            9,680円（税込）/月 <br>-オンラインルーム利用可能（予約制)
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>単発利用(2時間以上)</th>
+                        <td>
+                            1,650円（税込）/人 <br>-オープンスペースのみ
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>単発利用(2時間)</th>
+                        <td>
+                            550円（税込）/人 <br>-オープンスペースのみ
+                        </td>
+                    </tr>
+                </table>
+                <div class="front-price__desc">
+                    <p>※月額会員の申し込みは、入会金としてICカード１枚につき3,300円（税込）の事務手数料が発生します。</p>
+                    <p>※オンラインルームは、月額会員さまの利用希望が多いため、単発利用（ドロップイン）でのご利用はできません。</p>
+                    <p>※法人契約をご希望の方は、見学申し込みからお問い合わせください。</p>
+                </div>
+                <table class="front-price__table">
+                    <tr>
+                        <th>月額会員(※1)</th>
+                        <td>
+                            クレジットカード・口座振替（前払い）
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>単発利用</th>
+                        <td>
+                            現金・各種クレジットカード・QRコード決済など
+                        </td>
+                    </tr>
+                </table>
+
+                <div class="common-btn">
+                    <a href="https://forms.gle/dKHLvPUtbcNNEAfR6">見学申し込みはこちら</a>
+                </div>
+            </div>
+        </section>
+
+        <section class="front-map common-wrapper">
+            <div class="common-inner">
+                <div class="common-title">
+                    <span class="common-subtitle">Map</span>
+                    <h3 class="common-maintitle">住所</h3>
+                </div>
+                <p>〒790-0012　愛媛県松山市湊町4丁目6番地12 池田ビル1階2階</p>
+                <img src="img/map.jpg" alt="マップ画像">
+            </div>
+        </section>
+
+    </main>
+
+    <footer class="footer">
+        <p>COPYRIGHT©︎techis All rights reserved.</p>
+    </footer>
+</body>
+</html>
